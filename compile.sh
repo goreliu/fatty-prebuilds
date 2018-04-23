df476a93cf8c594965a14c6b5d568f689de5ac27
Gore Liu <goreliu@126.com>
2018-04-23 10:43:41 +0800

    Add doc and etc directories

--- compile.sh
+++ compile.sh
@@ -4,6 +4,8 @@ export PATH=/bin
 make
 strip src/fatty.exe
 
-mkdir -p fatty/bin
+mkdir -p fatty/{bin,doc,etc/lang}
 cp src/fatty.exe fatty/bin
 cp /bin/{cyggcc_s-seh-1.dll,cygstdc++-6.dll,cygwin-console-helper.exe,cygwin1.dll} fatty/bin
+cp COPYING LICENSE* README.md fatty/doc
+cp lang/*.po fatty/etc/lang
