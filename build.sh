b69cd0360cd43e691dada4d687b7823104c9138d
Gore Liu <goreliu@126.com>
2018-10-29 18:12:21 +0800

    Fix version

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ cd fatty
 
 /init /mnt/c/cygwin64/bin/bash.exe ../compile.sh
 
-version=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"-2)
+version=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"-1)
 rm -f ../fatty-$version.7z
 7z a ../fatty-$version.7z fatty
